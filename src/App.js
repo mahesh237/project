34ec03070dcd6ec9ead04f37a18febdd403e7eba
maheshbagwani <mahesh@appointy.com>
2022-01-06 22:50:33 +0530

    changesg

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoginForm from './components/login-form'
 import Navbar from './components/navbar'
 
 import './App.css';
-// import { runInContext } from 'vm';
+
 
 class App extends Component {
     constructor() {
@@ -84,18 +84,3 @@ class App extends Component {
 
 export default App;
 
-/* The above is a case of the need to pass a prop to a component being rendered by React Router, instead of using Routes component prop, use its render prop passing it an inline function then pass along the arguments to the element you’re creating.
-
-I could have done like below
-
-<Route
-        path='/login'
-        component={() => <LoginForm updateUser={this.updateUser />}
-        />
-
-The above would work technically but bad for performance - the reason is below :
-I pass an inline function to render the component within React Router because of performance. According to the offical docs… - “When you use the component props, the router uses React.createElement to create a new React element from the given component. That means if you provide an inline function to the component attribute, you would create a new component every render. This results in the existing component unmounting and the new component mounting instead of just updating the existing component.”
-
-See my detail note in - ../Curated-List-For-JavaScript-Interviews/React/pass-prop-to-component-rendered-by-React-Router.md
-
-*/
